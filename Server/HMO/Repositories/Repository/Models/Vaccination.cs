84c2e7ff15b3ce1427766944189d46a88e31f8b4
BatshevaYakovson <122361591+BatshevaYakovson@users.noreply.github.com>
2023-05-08 17:28:13 +0300

    server: add db context

---
+++ Server/HMO/Repositories/Repository/Models/Vaccination.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories.Repository.Models;
+
+[Table("Vaccination")]
+public partial class Vaccination
+{
+    [Key]
+    public int VaccinationId { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? Manufacturer { get; set; }
+}
